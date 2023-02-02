5bfa805675bec58cd2e7b2fc54bf07cd3c496686
ActivePeter <1020401660@qq.com>
2023-02-02 22:22:21 +0800

    use gorm model to create table

--- simple-demo/dal/init.go
+++ simple-demo/dal/init.go
@@ -4,6 +4,19 @@ import (
 	"github.com/RaymondCode/simple-demo/dal/mysql"
 )
 
+func createTables() error {
+	err := mysql.DB.AutoMigrate(
+		&Comment{},
+		&User{},
+		&FavouriteRelation{},
+		&VideoMeta{})
+	return err
+}
+
 func Init() {
 	mysql.Init()
+	err := createTables()
+	if err != nil {
+		return
+	}
 }
--- simple-demo/dal/table_models.go
+++ simple-demo/dal/table_models.go
@@ -1,1 +1,27 @@
 package dal
+
+//https://gorm.io/docs/models.html
+
+type User struct {
+	ID       int    `gorm:"primaryKey"`
+	Name     string `gorm:"type:varchar(255)"`
+	Username string `gorm:"type:varchar(32)"`
+	Password string `gorm:"type:varchar(32)"`
+}
+type VideoMeta struct {
+	ID       int `gorm:"primaryKey"`
+	Author   int
+	PlayUrl  string `gorm:"type:varchar(255)"`
+	CoverUrl string `gorm:"type:varchar(255)"`
+	Title    string `gorm:"type:varchar(255)"`
+}
+type FavouriteRelation struct {
+	UserID  int `gorm:"primaryKey;autoIncrement:false"`
+	VideoID int `gorm:"primaryKey;autoIncrement:false"`
+}
+type Comment struct {
+	Id      int `gorm:"primaryKey"`
+	Content string
+	UserID  int
+	VideoID int
+}
