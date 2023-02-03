170da2b3040be44ac1c21dfd5df55a8774160fc7
ActivePeter <1020401660@qq.com>
2023-02-03 21:46:51 +0800

    feed basicly ok

--- simple-demo/dal/video.go
+++ simple-demo/dal/video.go
@@ -1,8 +1,10 @@
 package dal
 
 import (
+	"fmt"
 	"github.com/RaymondCode/simple-demo/model"
 	"gorm.io/gorm"
+	"log"
 )
 
 type videoDBAPI struct{}
@@ -11,73 +13,66 @@ var DBVideo = videoDBAPI{}
 
 const onceFeedCnt = 30
 
-func (*videoDBAPI) videoFavCnt(vid int, tx *gorm.DB) (error, int) {
-	var cnt int
-	tx.Model(&FavouriteRelation{}).
-		Select("COUNT(user_id)").
-		Where("video_id= ?", vid).First(&cnt)
-	return tx.Error, cnt
-}
-func (*videoDBAPI) videoCommentsCnt(vid int, tx *gorm.DB) (error, int) {
-	var cnt int
-	tx.Model(&Comment{}).
-		Select("COUNT(id)").
-		Where("video_id= ?", vid).First(&cnt)
-	return tx.Error, cnt
-}
-func (*videoDBAPI) isUserFav(vid int, uid int, tx *gorm.DB) (error, bool) {
-	var cnt int
-	tx.Model(&FavouriteRelation{}).Select("CNT(user_id)").
-		Where("user_id=? AND video_id=?", uid, vid).First(&cnt)
-	return tx.Error, cnt == 1
-}
+//func (*videoDBAPI) videoFavCnt(vid int, tx *gorm.DB) (error, int) {
+//	var cnt int
+//	tx.Model(&FavouriteRelation{}).
+//		Select("COUNT(user_id)").
+//		Where("video_id= ?", vid).First(&cnt)
+//	return tx.Error, cnt
+//}
+//func (*videoDBAPI) videoCommentsCnt(vid int, tx *gorm.DB) (error, int) {
+//	var cnt int
+//	tx.Model(&Comment{}).
+//		Select("COUNT(id)").
+//		Where("video_id= ?", vid).First(&cnt)
+//	return tx.Error, cnt
+//}
+//func (*videoDBAPI) isUserFav(vid int, uid int, tx *gorm.DB) (error, bool) {
+//	var cnt int
+//	tx.Model(&FavouriteRelation{}).Select("CNT(user_id)").
+//		//Where("user_id=? AND video_id=?", uid, vid).
+//		First(&cnt)
+//	return tx.Error, cnt == 1
+//}
 
-func (v *videoDBAPI) SelectVideo(userid int, afterTime int64) (error, []model.Video) {
+func (*videoDBAPI) SelectVideo(userid int, afterTime int64) (error, []model.Video) {
 	res := make([]model.Video, 0)
 	err := DB.Transaction(func(tx *gorm.DB) error {
 		//1. 获取视频
-		rows, err := tx.Model(&VideoMeta{}).Limit(onceFeedCnt).Rows()
+		rows, err := tx.Debug().Model(&VideoMeta{}).Limit(onceFeedCnt).
+			Joins("left join users on video_meta.author=users.id").
+			Select("video_meta.id, video_meta.author, video_meta.play_url, video_meta.cover_url,"+ //video
+				"users.id, users.name, users.follow_count, users.follower_count,"+ //author
+				"exists (?),"+ //是否喜欢
+				"(?),"+ //喜爱数
+				"(?)", //评论数
+				tx.Model(&FavouriteRelation{}).
+					Where("user_id=? AND video_id=video_meta.id", userid),
+				tx.Model(&FavouriteRelation{}).
+					Where("video_id=video_meta.id").Select("COUNT(user_id)"),
+				tx.Model(&Comment{}).
+					Where("video_id=video_meta.id").Select("COUNT(id)"),
+			).Limit(onceFeedCnt).Rows()
 		if err != nil {
 			return err
 		}
-		if rows.Next() {
-			videoMeta := VideoMeta{}
-			err = rows.Scan(&videoMeta)
+		for rows.Next() {
+			v := model.Video{}
+			var author_id int
+			err := rows.Scan(&v.Id, &author_id, &v.PlayUrl, &v.CoverUrl,
+				&v.Author.Id, &v.Author.Name, &v.Author.FollowCount, &v.Author.FollowerCount,
+				&v.IsFavorite,
+				&v.FavoriteCount,
+				&v.CommentCount,
+			)
 			if err != nil {
+				log.Default().Printf("read video meta failed %v\n", err)
 				return err
 			}
-			err, favcnt := v.videoFavCnt(videoMeta.ID, tx)
-			if err != nil {
-				return err
-			}
-			err, cmtcnt := v.videoCommentsCnt(videoMeta.ID, tx)
-			if err != nil {
-				return err
-			}
-			isfav := false
-			// user 为登录状态，需要检查是否喜爱
-			if userid > -1 {
-				err, isfav_ := v.isUserFav(videoMeta.ID, userid, tx)
-				if err != nil {
-					return err
-				}
-				isfav = isfav_
-			}
-			author, err := UserBasicInfo(tx, videoMeta.Author)
-			if err != nil {
-				return err
-			}
-			res = append(res, model.Video{
-				Id:            int64(videoMeta.ID),
-				Author:        author,
-				PlayUrl:       videoMeta.PlayUrl,
-				CoverUrl:      videoMeta.CoverUrl,
-				FavoriteCount: int64(favcnt),
-				CommentCount:  int64(cmtcnt),
-				IsFavorite:    isfav,
-			})
+			res = append(res, v)
 		}
 		return nil
 	})
+	fmt.Printf("%+v\n", res)
 	return err, res
 }
--- simple-demo/go.mod
+++ simple-demo/go.mod
@@ -4,7 +4,6 @@ go 1.17
 
 require (
 	github.com/cloudwego/hertz v0.5.1
-	github.com/cloudwego/hertz-examples/bizdemo/hertz_jwt v0.0.0-20230129085900-a27807718aa9
 	github.com/gavv/httpexpect/v2 v2.8.0
 	github.com/hertz-contrib/jwt v1.0.0
 	github.com/stretchr/testify v1.7.1
@@ -23,10 +22,6 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
-	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/go-playground/locales v0.14.0 // indirect
-	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/golang-jwt/jwt/v4 v4.4.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
@@ -37,23 +32,20 @@ require (
 	github.com/imkira/go-interpol v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
-	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/nyaruka/phonenumbers v1.0.55 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/sanity-io/litter v1.5.5 // indirect
 	github.com/sergi/go-diff v1.0.0 // indirect
 	github.com/tidwall/gjson v1.13.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
-	github.com/ugorji/go/codec v1.2.7 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.34.0 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
@@ -63,11 +55,10 @@ require (
 	github.com/yudai/gojsondiff v1.0.0 // indirect
 	github.com/yudai/golcs v0.0.0-20170316035057-ecda9a501e82 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
-	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
-	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	moul.io/http2curl/v2 v2.3.0 // indirect
